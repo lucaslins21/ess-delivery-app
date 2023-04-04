803a9e6d8e19dc6159e729e7bb6db947e86a0e04
Wellinne <wwna@cin.ufpe.br>
2023-04-04 17:44:42 -0300

    conexão da tela de editar perfil com o back nos métodos: GET, DELETE e PUT

--- front/src/components/Botao/index.jsx
+++ front/src/components/Botao/index.jsx
@@ -7,7 +7,17 @@ export const Botao = (props) => {
         backgroundColor: props?.backgroundColor,
         color: props?.color,
         width: props?.width,
-        border: props?.border
+        border: props?.border,
+       }}
+       onMouseOut={(e) => {
+            e.target.style.backgroundColor = props?.backgroundColor;
+            e.target.style.color = props?.color;
+            e.target.style.border = props?.border;
+        }}
+       onMouseOver={(e) => {
+            e.target.style.backgroundColor = props?.backgroundColorHover;
+            e.target.style.color = props?.colorHover;
+            e.target.style.border = props?.borderHover;
         }}
         onClick={props?.onClick}
         >{props?.text}</button>
