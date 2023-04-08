96dc153dc39aa79c3c4ed022ee9fa02c1255364a
Paulo Souza <psgs@cin.ufpe.br>
2023-04-08 14:40:09 -0300

    criacao do back de login

--- server/src/controllers/UsuarioController.ts
+++ server/src/controllers/UsuarioController.ts
@@ -53,5 +53,30 @@ export default class UsuarioController implements Crud {
         return response.status(httpStatus).send({ messageFromUpdate });
     }
 
+
+    async login(request: Request, response: Response) {
+        const { email, senha } = request.body;
+        // Verifica se o email e a senha foram fornecidos
+        if (!email || !senha) {
+          return response.status(400).send({ mensagem: 'Email e senha são obrigatórios.' });
+        }
+      
+        // Busca o usuário pelo email
+        const usuario = await BuchoCheio.findByEmail(Usuario, email); 
+        console.log(usuario,email,senha);
+        // Verifica se o usuário foi encontrado
+        if (!usuario) {
+          return response.status(401).send({ mensagem: 'Usuário não encontrado.' });
+        }
+      
+        // Verifica se a senha fornecida corresponde à senha do usuário
+        if (usuario.value.senha !== senha) {
+          return response.status(401).send({ mensagem: 'Senha incorreta.' });
+        }
+      
+        // Se chegamos aqui, o login foi bem-sucedido
+        return response.status(200).send({ id: usuario.value.id });
+      }
+      
     
 }
