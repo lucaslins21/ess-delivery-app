04f20747147274c559ad23694a81524d0c5c4dc0
victoremanue <victorpdf7@gmail.com>
2023-04-08 14:35:54 -0300

    Criação do front do login admin

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,4 +1,5 @@
 import { Home } from './Home';
+import { Login } from './Login';
 import { EditarPerfil } from './EditarPerfil';
 
-export { Home, EditarPerfil };
+export { Home, EditarPerfil, Login };
