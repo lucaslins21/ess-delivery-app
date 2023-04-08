dd49029613527b4c0f6e42ad335948e2d1a4ab97
Wellinne <wwna@cin.ufpe.br>
2023-04-08 15:22:25 -0300

    criação da navbar

--- front/src/assets/index.js
+++ front/src/assets/index.js
@@ -1,5 +1,9 @@
 import  Logo  from './Logo.png';
+import { ReactComponent as IconPerfil } from './IconPerfil.svg';
+import { ReactComponent as IconBag } from './IconBag.svg';
 
 export {
     Logo,
+    IconPerfil,
+    IconBag,
 }
--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from '../components/Navbar';
 import { EditarPerfil, Home } from '../pages';
 
 export const PublicRoutes = () => (
@@ -7,6 +8,7 @@ export const PublicRoutes = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/perfil" element={<EditarPerfil />} />
+      <Route path="/navbar" element={<Navbar />} />
     </Routes>
   </BrowserRouter>
 );
