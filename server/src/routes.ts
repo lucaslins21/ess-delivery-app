c3d3755e3d89d86fc86452b93f8fd66caf642ecb
Lucas Lins <lucas.lemos61@gmail.com>
2023-04-10 15:43:46 -0300

    criação do banco de dados do cardapio

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import UsuarioController from '@controllers/UsuarioController'
+import ItemController from '@controllers/ItemController';
 
 const routes = express.Router();
 const usuarioController = new UsuarioController();
+const itemController = new ItemController();
 
 routes.post('/usuario', usuarioController.create);
 routes.get('/usuario', usuarioController.get);
@@ -10,5 +12,11 @@ routes.get('/usuario/:id', usuarioController.getById);
 routes.delete('/usuario/:id', usuarioController.delete);
 routes.put('/usuario/:id', usuarioController.update);
 routes.post('/login', usuarioController.login);
+
+routes.post('/item', itemController.create);
+routes.get('/item', itemController.get);
+routes.get('/item/:id', itemController.getById);
+routes.delete('/item/:id', itemController.delete);
+routes.put('/item/:id', itemController.update);
 
 export default routes;
