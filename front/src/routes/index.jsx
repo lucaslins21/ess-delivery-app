0b51f115dd44f03aebf23d7a40296c8f45bdee70
Wellinne <wwna@cin.ufpe.br>
2023-04-11 23:24:00 -0300

    mudando as rotas do login e ajeitando o css do projeto

--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -3,11 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navbar } from '../components/Navbar';
 import { EditarPerfil, Login, Carrinho, Cadastro } from '../pages';
 import {Home} from '../pages';
-//<Route path="/home" element={<Home />} />
+
 export const PublicRoutes = () => (
   <BrowserRouter>
     <Routes>
-
       <Route path="/home" element={<Home />} />
       <Route path="/perfil" element={<EditarPerfil />} />
       <Route path="/navbar" element={<Navbar />} />
